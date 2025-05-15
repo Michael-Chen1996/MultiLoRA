80e864a5589ec945e67b919e50124b5b5895f0a6
unknown <michaelchen1996@163.com>
2025-05-15 21:05:09 +0800

    feat: first runnable code

--- train.py
+++ train.py
@@ -26,7 +26,8 @@ test_dataset = Dataset.from_dict({'text': test_texts, 'emotion': test_emotions, 
 # 加载预训练模型和tokenizer
 model_name = 'BAAI/bge-large'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=16).to(device)
 
 # 配置LoRA
 lora_config = LoraConfig(
@@ -50,9 +51,19 @@ def preprocess_function(examples):
 train_dataset = train_dataset.map(preprocess_function, batched=True)
 test_dataset = test_dataset.map(preprocess_function, batched=True)
 
+# 定义collate_fn函数
+def collate_fn(batch):
+    return {
+        'input_ids': torch.stack([torch.tensor(item['input_ids']).to(device) for item in batch]),
+        'attention_mask': torch.stack([torch.tensor(item['attention_mask']).to(device) for item in batch]),
+        'emotion': torch.tensor([{'neutral': 0, 'angry': 1, 'happy': 2, 'frustrated': 3}[item['emotion']] for item in batch]).to(device),
+        'fulfilled': torch.tensor([item['fulfilled'] for item in batch], dtype=torch.float).to(device),
+        'type': torch.tensor([{'loan': 0, 'interest_rate': 1, 'savings_account': 2, 'fraud': 3, 'investment': 4, 'online_banking': 5, 'credit_card': 6, 'loan_repayment': 7, 'insurance': 8, 'account_update': 9, 'other': 10}[item['type']] for item in batch]).to(device)
+    }
+
 # 创建DataLoader
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=8)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=8, collate_fn=collate_fn)
 
 # 损失函数
 emotion_loss_fn = torch.nn.CrossEntropyLoss()
@@ -71,12 +82,16 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         
         # 前向传播
-        outputs = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'])
+        outputs = model(input_ids=batch['input_ids'].to(device), attention_mask=batch['attention_mask'].to(device))
+        logits = outputs.logits
+        emotion_logits = logits[:, :4]
+        fulfilled_logits = logits[:, 4]
+        type_logits = logits[:, 5:16]
         
         # 多任务损失计算
-        emotion_loss = emotion_loss_fn(outputs.logits[:, :5], batch['emotion'])
-        fulfilled_loss = fulfilled_loss_fn(outputs.logits[:, 5], batch['fulfilled'].float())
-        type_loss = type_loss_fn(outputs.logits[:, 6:], batch['type'])
+        emotion_loss = emotion_loss_fn(emotion_logits, batch['emotion'])
+        fulfilled_loss = fulfilled_loss_fn(fulfilled_logits, batch['fulfilled'].float())
+        type_loss = type_loss_fn(type_logits, batch['type'])
         
         loss = emotion_loss + fulfilled_loss + type_loss
         
